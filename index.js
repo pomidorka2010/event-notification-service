c7d750235aa7e1d8d6fcf6f668a892e920d2b889
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-06 20:56:22 +0200

    Added error handling for MongoDB connection

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/event-notification', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
 
 // Basic route
